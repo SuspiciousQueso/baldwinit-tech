cef58ce4ed9a64c012bae6d2be143946c6eaf9fb
Billy R Baldwin <billyraybaldwin@gmail.com>
2026-01-02 20:24:28 -0600

    First!

--- src/config.php
+++ src/config.php
@@ -5,6 +5,6 @@ return [
     'app_name'     => getenv('APP_NAME') ?: 'BaldwinIT',
     'app_url'      => getenv('APP_URL') ?: 'https://baldwinit.tech',
     'contact_email'=> getenv('CONTACT_EMAIL') ?: 'support@baldwinit.tech',
-    'ticket_url'   => getenv('TICKET_URL') ?: '',
+    'ticket_url'   => getenv('TICKET_URL') ?: 'https://mspguild.tech/modules/frontdesk/create.php',
     'booking_url'  => getenv('BOOKING_URL') ?: '',
 ];
